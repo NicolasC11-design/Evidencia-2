986a84eb582f733d479ba8345ecc3662c62a62ac
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2026-09-02 08:34:03 -0500

    first commit

--- app/app.py
+++ app/app.py
@@ -22,4 +22,4 @@ def health_check():
         return jsonify({"status": "unhealthy", "error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000) # nosec B104
